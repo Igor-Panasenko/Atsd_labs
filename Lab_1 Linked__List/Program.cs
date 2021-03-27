a525606e8f970a9dc1dedcd6ce26476140ff0f30
Igor-Panasenko <dgekichan2014@gmail.com>
2021-03-27 13:12:48 +0200

    add class Node

--- Lab_1 Linked__List/Program.cs
+++ Lab_1 Linked__List/Program.cs
@@ -2,6 +2,12 @@
 
 namespace Lab_1_Linked__List
 {
+    class Node {
+        protected int data;
+        protected Node head;
+
+    }
+
     class Program
     {
         static void Main(string[] args)
