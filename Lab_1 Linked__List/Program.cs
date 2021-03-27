39af99e7a3ba83e2fca77cbda779af8b6920f0a2
Igor-Panasenko <dgekichan2014@gmail.com>
2021-03-27 13:09:27 +0200

    Add new project for lab_1

---
+++ Lab_1 Linked__List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_1_Linked__List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
