eafe20cc227ee7c758254431d0a4289f240270c8
Igor-Panasenko <dgekichan2014@gmail.com>
2021-04-07 12:15:10 +0300

    add generic class Node

--- lab_2 Binary tree/Binary_tree/Program.cs
+++ lab_2 Binary tree/Binary_tree/Program.cs
@@ -2,6 +2,18 @@
 
 namespace Binary_tree
 {
+    class Node<T> {
+        public T Data;
+        public int Height;
+        public Node<T> Left;
+        public Node<T> Right;
+
+        public Node(T data){
+            this.Data = data;
+            Height = 1;
+            }
+    }
+
     class Program
     {
         static void Main(string[] args)
