209ae7f84aa343b5c14293810d1a7a2914cd7cd1
Igor-Panasenko <dgekichan2014@gmail.com>
2021-04-07 13:39:14 +0300

    add empty constructor

--- lab_2 Binary tree/Binary_tree/Program.cs
+++ lab_2 Binary tree/Binary_tree/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 
 
+
 namespace Binary_tree
 {
-    class Node<T> where T : IComparable<T> {
+    class Node<T> where T : IComparable {
         public T Data;
-        public int Height;
-        public Node<T> Left;
-        public Node<T> Right;
+        public int Height = 0;
+        public Node<T> Left = null;
+        public Node<T> Right = null;
 
         public Node(T data){
             this.Data = data;
             Height = 1;
             }
-
+        public Node() { }
     }
 
     class Binary_Tree<T>
@@ -26,6 +27,14 @@ namespace Binary_tree
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+          /*  Node<int> mu_1 = new Node<int>(10);
+            Node<string> my_2 = new Node<string>("342");
+            Node<double> my_3 = new Node<double>(32.2);
+            Node<int> my_4 = new Node<int>(113);
+            Node<string> my_5 = new Node<string>("89");
+            Node<double> my_6 = new Node<double>(17.2);
+            mu_1.Left = my_3;*/
         }
     }
 }
