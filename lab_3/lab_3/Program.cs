093aa317d82e20e7c206740a8f84a08938ee8699
Igor-Panasenko <dgekichan2014@gmail.com>
2021-04-27 13:22:35 +0300

    add value SizeOfHeap

--- lab_3/lab_3/Program.cs
+++ lab_3/lab_3/Program.cs
@@ -6,7 +6,18 @@ namespace lab_3
     class BinaryHeap<T> where T:IComparable {
 
         public T[] HeapArray;
-
+        private int sizeOfHeap;
+        public int SizeOfHeap
+        {
+            get
+            {
+                return sizeOfHeap;
+            }
+            set
+            {
+                sizeOfHeap = value;
+            }
+        }
     
     }
 
