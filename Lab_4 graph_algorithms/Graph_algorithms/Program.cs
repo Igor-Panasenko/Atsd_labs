3edde5c84b3a4da0c9a6804a80cac8e03cb08845
Igor-Panasenko <dgekichan2014@gmail.com>
2021-05-11 17:24:23 +0300

    create new project for lab_4

---
+++ Lab_4 graph_algorithms/Graph_algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Graph_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
