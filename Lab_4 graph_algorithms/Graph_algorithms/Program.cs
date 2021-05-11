7f42377989301fbea9253f1734641fb35a439473
Igor-Panasenko <dgekichan2014@gmail.com>
2021-05-11 18:07:15 +0300

    create class Edge in Weighted graph;

--- Lab_4 graph_algorithms/Graph_algorithms/Program.cs
+++ Lab_4 graph_algorithms/Graph_algorithms/Program.cs
@@ -3,7 +3,12 @@
 namespace Graph_algorithms
 {
     class WeightedGraph<T> where T : IComparable {
-    
+        private class Edge<T>:IComparable<Edge<T>> where T:IComparable {
+            public int src;
+            public int dest;
+            public T weight;
+        }
+
     }
 
     class Program
