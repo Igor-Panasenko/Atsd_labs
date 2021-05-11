3c43bdc7625c1f57335c28bb2d4132e44f2765e3
Igor-Panasenko <dgekichan2014@gmail.com>
2021-05-11 20:15:19 +0300

    added constructor for speaking

--- Lab_4 graph_algorithms/Graph_algorithms/Program.cs
+++ Lab_4 graph_algorithms/Graph_algorithms/Program.cs
@@ -4,19 +4,26 @@ namespace Graph_algorithms
 {
     public class WeightedGraph<T> where T : IComparable
     {
-        public class Edge<T> : IComparable<Edge<T>> where T : IComparable
+        public class Edge : IComparable<Edge>
         {
             public int src;
             public int dest;
-            public T weight;
+            public int weight;
 
             public Edge()
             {
                 src = 0;
                 dest = 0;
-                weight = default(T);
+                weight = 0;
             }
-            public int CompareTo(Edge<T> comparerEdge)
+
+            public Edge(int from, int to, int weight)
+            {
+                src = from;
+                dest = to;
+                this.weight =weight;
+            }
+            public int CompareTo(Edge comparerEdge)
             {
                 if (this.weight.CompareTo(comparerEdge.weight) > 0)
                 {
@@ -42,8 +49,8 @@ namespace Graph_algorithms
 
         private int V;
         private int E;
-        public Edge<T>[] edge;
-        int[] Adjacency_matrix;
+        public Edge[] edge;
+        int[,] Adjacency_matrix;
  public WeightedGraph()
         {
             Console.WriteLine("Hello, write number of edges and \n number of vertices in your graph");
@@ -73,7 +80,7 @@ namespace Graph_algorithms
             }
             this.V = V;
             this.E = E;
-            this.edge = new Edge<T>[E];
+            this.edge = new Edge[E];
             int max_number = V + 2;
             int number = 0;
             while (number != E)
@@ -82,6 +89,7 @@ namespace Graph_algorithms
                 str=Console.ReadLine();
                 te = 0;
                 part_str = "";
+                
                 while (str[te] != ',')
                 {
                     part_str += str[te];
@@ -98,17 +106,27 @@ namespace Graph_algorithms
                 int to = Convert.ToInt32(part_str);
                 Console.WriteLine("write weight of this vertex");
                 int weight= Convert.ToInt32(Console.ReadLine());
-                Edge
+                edge[number] = new Edge(from, to, weight);
+                Console.WriteLine("if you want to end, press'#'");
+                string for_break = Console.ReadLine();
+                if (for_break == "#")
+                {
+                    break;
+                }
+                for(int i=0; i<edge.Length; i++)
+                {
+                    Adjacency_matrix[edge[i].src, edge[i].dest] = edge[i].weight;
+                }
             }
         }
         public WeightedGraph(int v, int e)
         {
             this.V = v;
             this.E = e;
-            this.edge = new Edge<T>[E];
+            this.edge = new Edge[E];
             for (int i = 0; i < e; i++)
             {
-                edge[i] = new Edge<T>();
+                edge[i] = new Edge();
             }
         }
         private int find(subset[] subsets, int i)
@@ -143,16 +161,16 @@ namespace Graph_algorithms
         }
         public void KruskalsMST()
         {
-            Edge<T>[] result = new Edge<T>[this.V];    //массив где хранится результирующее МСТ
+            Edge[] result = new Edge[this.V];    //массив где хранится результирующее МСТ
             int e = 0;
             for (int i = 0; i < V; ++i)
             {
-                result[i] = new Edge<T>();                 //заполнили результат дефолтными полями 
+                result[i] = new Edge();                 //заполнили результат дефолтными полями 
             }
-            Edge<T>[] copy = new Edge<T>[this.edge.Length];
+            Edge[] copy = new Edge[this.edge.Length];
             for (int i = 0; i < this.edge.Length; ++i)
             {
-                copy[i] = new Edge<T>();                
+                copy[i] = new Edge();                
             }
             for (int j = 0; j < this.edge.Length; j++)
             {
@@ -166,7 +184,7 @@ namespace Graph_algorithms
                 {
                     if (copy[j].CompareTo(copy[k]) > 0)
                     {
-                        Edge<T> temp = copy[j];
+                        Edge temp = copy[j];
                         copy[j] = copy[k];
                         copy[k] = temp;
                     }
@@ -185,7 +203,7 @@ namespace Graph_algorithms
             int next = 0;
             while (e < V - 1)
             {
-                Edge<T> next_edge = copy[next];
+                Edge next_edge = copy[next];
                 int x = find(subsets, next_edge.src);
                 int y = find(subsets, next_edge.dest);
 
