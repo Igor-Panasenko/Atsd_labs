6080dc4f82599d0e40c583b19ea530501687c3eb
Igor-Panasenko <dgekichan2014@gmail.com>
2021-05-27 21:51:36 +0300

    added 2 constructors for Node

--- Lab_5/Zadanie for lab_5/Program.cs
+++ Lab_5/Zadanie for lab_5/Program.cs
@@ -2,10 +2,14 @@
 
 namespace Zadanie_for_lab_5
 {
-    public class Node<T> where T : IComparable { 
-    public T Data;
+    public class Node<T> where T : IComparable {
+        public T Data;
         public Node<T> Left = null;
         public Node<T> Right = null;
+        public Node(T data){
+            this.Data = data;
+        }
+        public Node() { }
     }  
 
     class Program
